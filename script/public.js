1cc0464c702a5a6036931cb29768c577ec56dd32
SnowMu <102015170+Zs103n129@users.noreply.github.com>
2023-12-17 02:29:18 +0000

    完成honkai-impart-3rd部分

---
+++ script/public.js
@@ -0,0 +1,28 @@
+function wheelturnpage(max) {
+  let scrollPosition = 0; // 设置当前滚动位置
+  // 监听滚轮事件
+  window.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) {
+      // 判断滚轮方向
+      scrollPosition++; // 向下滚动，滚动位置加一
+    } else {
+      scrollPosition--; // 向上滚动，滚动位置减一
+    }
+
+    if (scrollPosition > max) {
+      scrollPosition = 1;
+    } //超出时返回
+    if (scrollPosition < 0) {
+      scrollPosition = 0;
+    }
+
+    let scrollDistance = window.innerHeight * scrollPosition; // 计算要滚动的距离（100vh）
+    window.scrollTo(
+      // 滚动到指定位置
+      {
+        top: scrollDistance,
+        behavior: "smooth",
+      }
+    );
+  });
+}
