422a37b175f0cd913f093a2555e8605a93f163c5
SnowMu <102015170+Zs103n129@users.noreply.github.com>
2023-12-18 16:30:21 +0800

    完成hongkai_3rd.html

--- script/honkai-impart-3rd.js
+++ script/honkai-impart-3rd.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-  throttle(wheelturnpage(9), 500);
-  wheelturnpage(9);
+  throttle(wheelturnpage(10), 500);
+  wheelturnpage(10);
 
   const finally_b = document.getElementById("finally_educate_buttom");
   const Seele_b = document.getElementById("Seele_buttom");
@@ -107,5 +107,53 @@ window.onload = function () {
   Da_Capo_play.addEventListener("click", function () {
     Da_Capo_play.style.display = "none";
     Da_Capo_play.children[0].pause();
+  });
+
+  const information = document.getElementById("information");
+  const left = document.getElementsByClassName("left")[0];
+  const right = document.getElementsByClassName("right")[0];
+
+  let mark = 0;
+
+  function revise() {
+    if (mark == -1) {
+      mark = 4;
+    }
+    if (mark == 5) {
+      mark = 0;
+    }
+  }
+
+  function initilize() {
+    information.children[0].id = "";
+    information.children[1].id = "";
+    information.children[2].id = "";
+    information.children[3].id = "";
+    information.children[4].id = "";
+  }
+
+  function cut() {
+    initilize();
+    revise();
+    information.children[mark].id = "center";
+    mark = mark - 1;
+    revise();
+    information.children[mark].id = "left";
+    mark = mark + 1;
+    revise();
+    mark = mark + 1;
+    revise();
+    information.children[mark].id = "right";
+    mark = mark - 1;
+    revise();
+  }
+
+  left.addEventListener("click", function () {
+    mark = mark - 1;
+    cut();
+  });
+  right.addEventListener("click", function () {
+    mark = mark + 1;
+    cut();
   });
 };
